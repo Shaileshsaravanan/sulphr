fe482990f75272338d7e3b4c9d47f06379f55fe7
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-11-16 16:32:45 +0530

    vercel test

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 from supabase import create_client, Client
 from environs import Env
+import google.generativeai as genai
 import os
 
 app = Flask(__name__)
@@ -8,6 +9,9 @@ env = Env()
 env.read_env()
 url = env.str('SUPABASE_URL')
 key = env.str('SUPABASE_KEY')
+gemini_api = env.str('GEMINI_API')
+genai.configure(api_key=gemini_api)
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 supabase: Client = create_client(url, key)
 
 @app.route('/')
@@ -31,9 +35,15 @@ def api_sign_up():
     }).execute()
     return {'status': 'success'}
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/gemini', methods=['POST'])
+def gemini():
+    print('gemini')
+    data = request.get_json()
+    prompt = data.get('prompt')
+    print(prompt)
+    response = model.generate_content(prompt)
+    print(response.text)
+    return {'status': 'success', 'reply': response.text}
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
